7d88e849c44483971a2bd8b4a9e16c48f7d302be
Fernanda Gonzalez <fergch97@gmail.com>
2024-12-20 14:13:30 -0600

    basic view with button & state

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,68 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+void main() {
+  runApp(MyApp());
+}
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Hello Word App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  var history = [];
+
+  void generate() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  void increment() {
+    history.add(current);
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Random word:'),
+          Text(appState.current.asLowerCase),
+
+          Text('History'),
+          Text(appState.history.join(',')),
+
+          ElevatedButton(
+            onPressed: () {
+              appState.increment();
+              appState.generate();
+            },
+            child: Text('New word'),
+          ),
+        ],
+      ),
+    );
+  }
 }
